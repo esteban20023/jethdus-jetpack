50aad1645537d5417c1709907f8b92917b4b32db
esteban20023 <105112596+esteban20023@users.noreply.github.com>
2023-05-13 11:32:28 -0200

    Add files via upload

--- basicscodelab/MainActivity.kt
+++ basicscodelab/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -137,18 +138,15 @@ private fun CardContent(name: String) {
                 )
             }
         }
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(
-                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
-                contentDescription = if (expanded) {
-                    stringResource(R.string.show_less)
-                } else {
-                    stringResource(R.string.show_more)
-                }
-            )
+
+        ElevatedButton(
+            onClick = { expanded = !expanded }
+        ) {
+            Text(if (expanded) "Show less" else "Show more")
         }
     }
 }
+
 
 @Preview(
     showBackground = true,
